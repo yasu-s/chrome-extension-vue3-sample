e2cd1629f0f34630f327df373ae87dedd66b1c94
Yasuyuki Saito <ya_saito@uluru.jp>
2022-06-26 17:49:00 +0900

    色変更追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const color = ref('#000000')
+
+/*global chrome */
+async function changeColor(color: string) {
+  console.log('color: ' + color)
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id || 0 },
+    args: [color],
+    func: (args) => {
+      document.body.style.background = args
+    },
+  })
+}
+</script>
+
 <template>
   <div class="HomeView">
-    <span>home</span>
+    <h2>home</h2>
+    <div>
+      <input v-model="color" type="color" @update:model-value="changeColor($event)" />
+    </div>
   </div>
 </template>
